c5c3dfef1222f3e00bd3672632ab8bed4cb2b2e5
Madhur <madhur.ahuja@games24x7.com>
2022-02-17 23:09:03 +0530

    initial commit

---
+++ .config/picom/launch.sh
@@ -0,0 +1,11 @@
+!/usr/bin/env sh
+
+ Terminate already running picom instances
+killall -q picom
+
+ Wait until the processes have been shut down
+while pgrep -u $UID -x picom > /dev/null; do sleep 1; done
+
+picom --config ~/.config/picom/picom.conf -b --experimental-backends
+
+echo "Compton launched..."
